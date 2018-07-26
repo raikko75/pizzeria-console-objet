814fbcb5412dabe82bdee639e919d00c9ffa6425
test <robillardbastian@outlook.fr>
2018-07-26 12:29:19 +0200

    exception UpdatePizza

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,14 +1,34 @@
 package fr.pizzeria.console;
-
+import fr.pizza.dao.PizzaArrayDao;
+import fr.pizza.services.*;
 import java.util.Scanner;
+
+import fr.pizzeria.excetpion.StockageException;
+import fr.pizzeria.excetpion.UpdatePizzaException;
+import fr.pizzeria.model.Pizza;
 
 public class PizzeriaAdminConsoleApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws StockageException {
 		// TODO Auto-generated method stub
 		boolean demarrage = true;
 		int choixUser;
+		 Scanner choix = new Scanner(System.in);
+		 PizzaArrayDao pizza = new PizzaArrayDao();
+		 MenuServiceFactory c = new MenuServiceFactory();
+		pizza.arrayPizzaDao();
 
+		// alimenter un tableau s
+		/*
+		tableauPizza [0]=  new Pizza(0, "pep","peperoni",12.50);
+		tableauPizza [1] = new Pizza(1, "mar","margueria",14.00);
+		tableauPizza [2] = new Pizza(2, "rein","la reine",11.50);
+		tableauPizza [3] = new Pizza(3, "fro","les 4 fromages",12.00);
+		tableauPizza [4] = new Pizza(4, "can","la cannibale",12.50);
+		tableauPizza [5] = new Pizza(5, "sav","savoyarde",13.00);
+		tableauPizza [6] = new Pizza(6, "ori","oriantale",13.50);
+		tableauPizza [7] = new Pizza(7, "ind","indienne",14.00);
+*/
 		while (demarrage = true)
 		{
 			System.out.println("**************Pizzeria Administration***************");
@@ -17,31 +37,111 @@ public class PizzeriaAdminConsoleApp {
 			System.out.println("choix 3 Mettre à jour une pizza ");
 			System.out.println("choix 4 supprimer une pizza");
 			System.out.println("choix 99 sortir");
-			
-			Scanner choix = new Scanner(System.in);
-			choixUser = choix.nextInt();
-			
-			if (choixUser == 1)
-			{
-				System.out.println("***liste des pizzas****");
+
+			try {
+				choixUser = choix.nextInt();
+				MenuService nn = c.getChoixMenu(choixUser);
+				nn.executeUC(pizza, choix);
+				
+			} catch (StockageException e) {
+				System.out.println("code inexistant");
 			}
-				else if(choixUser == 2)
+				/*
+				if(codePizzatemp.isEmpty() )
 				{
-					System.out.println("***ajout d'une nouvelle pizza *** ");
-				}
-				else if (choixUser == 3)
+					throw new UpdatePizzaException("le champ est vide");
+				}			
+				if(arrayPizza.get(i).getCode().equals(codePizzatemp))
 				{
-					System.out.println("***Mis a jour de la pizza***");
+					throw new UpdatePizzaException("le champ est vide");
 				}
-				else if(choixUser == 4)
-				{
-					System.out.println("***suppresion de la pizza selectionne***");
-					
+				
 				}
-				else if (choixUser == 99) {
-					System.out.println(" bye bye :( :( ");
-				}
+				*/
+			
+
+			//*****************************************************************************************************
+			//*****************************************************************************************************
+			//*****************************************************************************************************
+
+			//*****************************************************************************************************
+
+
+
 		}
 	}
-
 }
+
+
+/*
+	public static void showPizza()
+	{
+		for (Pizza temp : tableauPizza)
+		{
+			System.out.println(temp.code + " > " + temp.libelle + " > " + temp.prix);
+		}
+	}
+*/
+	/*
+	public static void editPizza()
+	{
+		showPizza();
+		choix.nextLine();
+		System.out.println("veuillez saisir le code ");
+		String codePizzatemp = choix.nextLine();
+		System.out.println("veuillez saisir le nouveau code ");
+		String newCode = choix.nextLine();
+		System.out.println("veuillez saisir le nouveau nom(sans espace ");
+		String newname = choix.nextLine();
+		System.out.println("veuillez saisir le nouveau prix");
+		double choixPriceUsers = Double.parseDouble(choix.nextLine());
+		boolean findCodePizza = false;
+
+		for (int i=0;i<tableauPizza.length;i++)
+		{
+			if(tableauPizza[i].code.equals(codePizzatemp))
+			{
+				tableauPizza[i] = new Pizza( newCode, newname, choixPriceUsers);
+
+			}
+		}
+
+	}
+*/
+	/******************************************************************/
+	/******************************************************************/
+
+
+
+	/**********************************************************************/
+	/**********************************************************************/
+	/**********************************************************************/
+/*
+	public static void removePizza()
+	{
+		choix.nextLine();
+		System.out.println("le code pizza : ");
+		String newCodePizza = choix.nextLine();
+		Pizza newMenu [] = new Pizza[tableauPizza.length-1];
+		int temp =0;
+		for(int i=0;i<tableauPizza.length;i++)
+		{
+			if(!tableauPizza[i].code.equals(newCodePizza))
+			{	
+
+				newMenu[temp] = tableauPizza[i];
+				temp++;
+
+			}
+			else {
+				System.out.println("code inexistant");
+			}
+		}
+		tableauPizza = newMenu ;
+*/
+	
+
+
+
+
+
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -7,18 +7,49 @@ public class Pizza {
  */
 	
 	// variable d'instances
-	int id;
-	String code;
-	String libelle;
-	double prix;
-	
-	public Pizza ( String code , String libelle , double prix)
-	{
+	public int id=0;
+	public String code;
+	public String libelle;
+	public double prix;
+	public Pizza(int id, String code, String libelle, double prix) {
 		
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
 	}
-	
-	public Pizza (int id , String code , String libelle, double prix)
-	{
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public Pizza(String code, String libelle, double prix) {
 		
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	public void nombrePizza()
+	{
+		this.id = id++;
 	}
 }
